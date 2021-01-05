21114c568557f3d514ccbcda514570cd8d9a0f67
m3rl1n <m3rl1n@th31nitiate.xyz>
2021-01-05 17:02:55 +0000

    String features inlcuding some aspects of ui funcations

--- WindowsAPIDemos/WindowsAPIDemos.cpp
+++ WindowsAPIDemos/WindowsAPIDemos.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <Windows.h>
 #include <stdio.h>
+#include <strsafe.h>
 
 
 int Error(const char* msg) {
@@ -24,13 +25,27 @@ int main()
     printf("Maximum process address: 0x%p\n", si.lpMaximumApplicationAddress);
 
 
-    HANDLE hProcess = ::OpenProcess(PROCESS_TERMINATE, FALSE, 2552);
-    if (hProcess) {
-        ::TerminateProcess(hProcess, 0);
-    }
-    else
-        return Error("Failed to open process");
+    WCHAR buffer[128];
+    ::StringCchPrintf(buffer, _countof(buffer), L"This is my string in process %u :: %u :: %u", ::GetCurrentProcessId(), sizeof(buffer), _countof(buffer));
+    ::MessageBox(nullptr, buffer, L"String Demo", MB_OK | MB_ICONINFORMATION);
 
-        return 0;
+    WCHAR path[MAX_PATH];
+    ::GetSystemDirectory(path, _countof(path));
+    printf("System directory: %ws\n", path);
+
+
+    WCHAR computerName[MAX_COMPUTERNAME_LENGTH];
+    DWORD len = _countof(computerName);
+    if (::GetComputerName(computerName, &len))
+        printf("Computer name: %ws (%u)\n", computerName, len);
+
+    ///HANDLE hProcess = ::OpenProcess(PROCESS_TERMINATE, FALSE, 2552);
+    //if (hProcess) {
+    //    ::TerminateProcess(hProcess, 0);
+    //}
+    //else
+    //    return Error("Failed to open process");
+    
+    return 0;
 }
 
