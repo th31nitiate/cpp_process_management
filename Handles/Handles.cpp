fdfd700b82daef33259a8c540e7be83ad5500604
m3rl1n <m3rl1n@th31nitiate.xyz>
2021-01-06 13:59:55 +0000

    Ensure that handles are sharable between processes and verify way to implement this

--- Handles/Handles.cpp
+++ Handles/Handles.cpp
@@ -1,20 +1,28 @@
 // Handles.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <iostream>
 #include <Windows.h>
-
+#include <stdio.h>
+#include <string>
 
 
 int main()
 {
-    HANDLE hEvent = ::CreateEvent(nullptr, TRUE, FALSE, L"InitialEvent");
-    if (hEvent == nullptr)
-        printf("Failed to create event (%u)\n", ::GetLastError());
-    else {
-        ::Sleep(INFINITE);
-        ::SetEvent(hEvent);
-        ::CloseHandle(hEvent);
+    HANDLE hEvent = ::CreateEvent(nullptr, TRUE, FALSE, nullptr);
+    printf("HANDLE: 0x%p\n", hEvent);
+
+    PROCESS_INFORMATION pi;
+    STARTUPINFO si = { sizeof(si) };
+    //WCHAR name[] = L"Notepad"; //Will search system paths to try find notepad if full path not provided
+    std::wstring name(L"Notepad "); //Use this to assist in passing the handle between process to enabel sharing
+    name += std::to_wstring((long long)hEvent);
+
+    ::SetHandleInformation(hEvent, HANDLE_FLAG_INHERIT, HANDLE_FLAG_INHERIT);
+
+    if (::CreateProcess(nullptr, (WCHAR*)name.data(), nullptr, nullptr, TRUE, 0, nullptr, nullptr, &si, &pi)) {
+        printf("PID: %u\n", pi.dwProcessId);
+        ::CloseHandle(pi.hProcess);
+        ::CloseHandle(pi.hThread);
     }
 
    //:SetPriorityClass(::GetCurrentProcess(), HIGH_PRIORITY_CLASS);
